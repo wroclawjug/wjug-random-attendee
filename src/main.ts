a3df93591eef4969df125d6023a201a2c08bd7bf
WrRaThY <wontsay@gtfo.com>
2017-05-22 23:46:58 +0200

    some stupid flag to enable prod mode

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import {enableProdMode} from "@angular/core";
+
+if (true) { //TODO: wtf am I doing?
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
