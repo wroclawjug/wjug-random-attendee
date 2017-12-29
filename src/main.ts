955e47ebdd535ef7cf61ca9bde4bdf4d570e5d0e
Piotr Procner <piotrek.procner@gmail.com>
2017-12-29 12:37:01 +0100

    Changes build system to Angular CLI.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import {enableProdMode} from "@angular/core";
 
-if (true) { //TODO: wtf am I doing?
-  enableProdMode();
-}
-
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
