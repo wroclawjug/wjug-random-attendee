6a08bdd7188bf1bb3bd3ca44d812f67163e8257d
Artur Frysiak <artur@frysiak.net>
2019-09-04 20:14:09 +0200

    Fix deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,9 +2,11 @@
 set -e
 
 git clone https://wroclawjug:${github_token}@github.com/wroclawjug/wroclawjug.github.io.git
+
+set -x
 cd wroclawjug.github.io
 rm -r losowarka/*
-cp ../dist/* losowarka/
+cp -a ../dist/* losowarka/
 git add -A
 git commit -m "Update losowarka" || true
 git push
